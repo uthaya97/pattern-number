b2bd886abfb218a4fc5f79c7946b11f48f499bad
paulkani <paulkani.s@golden-hippo.com>
2023-07-18 12:05:06 +0530

    pattern number

---
+++ main.js
@@ -0,0 +1,10 @@
+var n=prompt("enter n value");
+for(let i=1;i<=n;i++)
+       	{
+	for(let j=1;j<=i;j++)
+	{
+	document.write(i);
+	document.write(" ");
+         }
+	document.write("<br>");
+         }
